88573a53374e273a26012fa48eae4b5486ac8a21
Steve Downey <sdowney2@bloomberg.net>
2024-04-25 19:44:09 -0400

    Clean up example code

--- src/examples/map_example.cpp
+++ src/examples/map_example.cpp
@@ -4,15 +4,15 @@
 namespace N::hidden {
 template <typename T>
 concept has_eq = requires(T const& v) {
-  { eq(v, v) } -> std::same_as<bool>;
+  { operator==(v, v) } -> std::same_as<bool>;
 };
 
 constexpr inline struct partial_eq_default_t {
-    constexpr bool eq_(has_eq auto const& rhs, has_eq auto const& lhs) const {
-        return eq(rhs, lhs);
+    constexpr bool eq(has_eq auto const& rhs, has_eq auto const& lhs) const {
+        return (rhs == lhs);
     }
-    constexpr bool ne_(has_eq auto const& rhs, has_eq auto const& lhs) const {
-        return not eq(lhs, rhs);
+    constexpr bool ne(has_eq auto const& rhs, has_eq auto const& lhs) const {
+        return (lhs != rhs);
     }
 } partial_eq_default;
 
@@ -26,8 +26,8 @@ constexpr inline auto partial_eq = hidden::partial_eq_default;
 template<class T>
 concept partial_equality =
     requires(std::remove_reference_t<T> const& t) {
-    {partial_eq<T>.eq_(t, t)} -> std::same_as<bool>;
-    {partial_eq<T>.ne_(t, t)} -> std::same_as<bool>;
+    {partial_eq<T>.eq(t, t)} -> std::same_as<bool>;
+    {partial_eq<T>.ne(t, t)} -> std::same_as<bool>;
 };
 } // namespace
 
@@ -35,16 +35,14 @@ concept partial_equality =
 struct test {
     int id;
 
-    friend bool eq(const test& t1, const test& t2) {
-        return t1.id == t2.id;
-    }
+    friend auto operator<=>(const test&, const test&) = default;
 };
 
 int main() {
     static_assert(N::partial_equality<test>);
 
     test t1, t2;
-    return N::partial_eq<test>.ne_(t1, t2);
+    return N::partial_eq<test>.ne(t1, t2);
 }
 
 // https://compiler-explorer.com/z/48E7osfxE
