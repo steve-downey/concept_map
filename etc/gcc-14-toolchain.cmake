cffbf1a046a631df45d6de18c4ef950e9a755fb8
Steve Downey <sdowney@gmail.com>
2024-04-25 21:09:35 -0400

    Enable assert

--- etc/gcc-14-toolchain.cmake
+++ etc/gcc-14-toolchain.cmake
@@ -10,7 +10,7 @@ set(CMAKE_CXX_FLAGS
 CACHE STRING "CXX_FLAGS" FORCE)
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -fno-inline -g3" CACHE STRING "C++ DEBUG Flags" FORCE)
-set(CMAKE_CXX_FLAGS_RELEASE "-Ofast -g0 -DNDEBUG" CACHE STRING "C++ Release Flags" FORCE)
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g -DNDEBUG" CACHE STRING "C++ RelWithDebInfo Flags" FORCE)
-set(CMAKE_CXX_FLAGS_TSAN "-O3 -g -DNDEBUG -fsanitize=thread" CACHE STRING "C++ TSAN Flags" FORCE)
-set(CMAKE_CXX_FLAGS_ASAN "-O3 -g -DNDEBUG -fsanitize=address,undefined,leak" CACHE STRING "C++ ASAN Flags" FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE "-Ofast -g0 " CACHE STRING "C++ Release Flags" FORCE)
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g " CACHE STRING "C++ RelWithDebInfo Flags" FORCE)
+set(CMAKE_CXX_FLAGS_TSAN "-O3 -g -fsanitize=thread" CACHE STRING "C++ TSAN Flags" FORCE)
+set(CMAKE_CXX_FLAGS_ASAN "-O3 -g -fsanitize=address,undefined,leak" CACHE STRING "C++ ASAN Flags" FORCE)
