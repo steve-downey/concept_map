9cc29fdbb3b59c41c5f85634520db573899d94d2
Steve Downey <sdowney@gmail.com>
2025-11-03 16:01:25 -0500

    Fix my dissatisfaction with formatting
    Update .clang-format and reformat everything.

--- src/examples/cpo_example.cpp
+++ src/examples/cpo_example.cpp
@@ -3,32 +3,33 @@
 
 namespace N::hidden {
 template <typename T>
-concept has_eq = requires(const T& v) {
-    { eq(v, v) } -> std::same_as<bool>;
+concept has_eq = requires(const T &v) {
+  {
+    eq(v, v)
+  } -> std::same_as<bool>;
 };
 
 struct eq_fn {
-    template <has_eq T>
-    constexpr bool operator()(const T& x, const T& y) const {
-        return eq(x, y);
-    }
+  template <has_eq T> constexpr bool operator()(const T &x, const T &y) const {
+    return eq(x, y);
+  }
 };
 
-template <has_eq T>
-constexpr bool ne(const T& x, const T& y) {
-    return not eq(x, y);
+template <has_eq T> constexpr bool ne(const T &x, const T &y) {
+  return not eq(x, y);
 }
 
 template <typename T>
-concept has_ne = requires(const T& v) {
-    { ne(v, v) } -> std::same_as<bool>;
+concept has_ne = requires(const T &v) {
+  {
+    ne(v, v)
+  } -> std::same_as<bool>;
 };
 
 struct ne_fn {
-    template <has_ne T>
-    constexpr bool operator()(const T& x, const T& y) const {
-        return ne(x, y);
-    }
+  template <has_ne T> constexpr bool operator()(const T &x, const T &y) const {
+    return ne(x, y);
+  }
 };
 } // namespace N::hidden
 
@@ -39,21 +40,21 @@ inline constexpr hidden::ne_fn ne{};
 } // namespace function_objects
 
 template <typename T>
-concept equality_comparable = requires(const std::remove_reference_t<T>& t) {
-    eq(t, t);
-    ne(t, t);
+concept equality_comparable = requires(const std::remove_reference_t<T> &t) {
+  eq(t, t);
+  ne(t, t);
 };
 } // namespace N
 
 struct test {
-    std::string id;
+  std::string id;
 
-    friend bool eq(const test& t1, const test& t2) { return t1.id == t2.id; }
+  friend bool eq(const test &t1, const test &t2) { return t1.id == t2.id; }
 };
 
 int main() {
-    static_assert(N::equality_comparable<test>);
+  static_assert(N::equality_comparable<test>);
 
-    test t1, t2;
-    return N::ne(t1, t2);
+  test t1, t2;
+  return N::ne(t1, t2);
 }
--- src/examples/map_example.cpp
+++ src/examples/map_example.cpp
@@ -3,13 +3,19 @@
 
 namespace N::hidden {
 template <typename T>
-concept has_eq = requires(const T& v) {
-    { operator==(v, v) } -> std::same_as<bool>;
+concept has_eq = requires(const T &v) {
+  {
+    operator==(v, v)
+  } -> std::same_as<bool>;
 };
 
 inline constexpr struct partial_eq_default_t {
-    constexpr bool eq(const has_eq auto& rhs, const has_eq auto& lhs) const { return (rhs == lhs); }
-    constexpr bool ne(const has_eq auto& rhs, const has_eq auto& lhs) const { return (lhs != rhs); }
+  constexpr bool eq(const has_eq auto &rhs, const has_eq auto &lhs) const {
+    return (rhs == lhs);
+  }
+  constexpr bool ne(const has_eq auto &rhs, const has_eq auto &lhs) const {
+    return (lhs != rhs);
+  }
 } partial_eq_default;
 
 } // namespace N::hidden
@@ -20,23 +26,27 @@ template <class T>
 inline constexpr auto partial_eq = hidden::partial_eq_default;
 
 template <class T>
-concept partial_equality = requires(const std::remove_reference_t<T>& t) {
-    { partial_eq<T>.eq(t, t) } -> std::same_as<bool>;
-    { partial_eq<T>.ne(t, t) } -> std::same_as<bool>;
+concept partial_equality = requires(const std::remove_reference_t<T> &t) {
+  {
+    partial_eq<T>.eq(t, t)
+  } -> std::same_as<bool>;
+  {
+    partial_eq<T>.ne(t, t)
+  } -> std::same_as<bool>;
 };
 } // namespace N
 
 struct test {
-    int id;
+  int id;
 
-    friend auto operator<=>(const test&, const test&) = default;
+  friend auto operator<=>(const test &, const test &) = default;
 };
 
 int main() {
-    static_assert(N::partial_equality<test>);
+  static_assert(N::partial_equality<test>);
 
-    test t1, t2;
-    return N::partial_eq<test>.ne(t1, t2);
+  test t1, t2;
+  return N::partial_eq<test>.ne(t1, t2);
 }
 
 // https://compiler-explorer.com/z/48E7osfxE
